0823c494de4a19071199e3b1647bf8e84b182792
Reynaldi Irfan Anwar <reynaldi.i.anwar@banksinarmas.com>
2023-02-13 12:36:24 +0700

    adding more component

---
+++ src/components/field_text.js
@@ -0,0 +1,11 @@
+import Form from 'react-bootstrap/Form';
+
+function FieldText() {
+  return (
+    <>
+      <Form.Control type="text" placeholder="Normal text" />
+    </>
+  );
+}
+
+export default FieldText;
