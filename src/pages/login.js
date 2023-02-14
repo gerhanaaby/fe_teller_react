ab2777deae699e890d9d389edf326c687046da34
DESKTOP-M6V98NJ\THINKPAD <gerhanaabiwow@gmail.com>
2023-02-14 09:50:48 +0700

    conflict

--- src/pages/login.js
+++ src/pages/login.js
@@ -8,72 +8,64 @@ import FieldText from "../components/field_text";
 import NavbarReact from "../components/navbar";
 import "./../css/login.css";
 
-import React from 'react'
+import React from "react";
 
 export default function Login() {
-    const [values, setValues] = useState({
-      userName: "",
-      pass: "",
-      showPass: false,
+  const [values, setValues] = useState({
+    userName: "",
+    pass: "",
+    showPass: false,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://reqres.in/api/login", {
+        userName: values.userName,
+        password: values.pass,
+      })
+      .then((res) => {
+        localStorage.setItem("token", res.data.token);
+      })
+      .catch((err) => console.error(err));
+  };
+  const handlePassVisibilty = () => {
+    setValues({
+      ...values,
+      showPass: !values.showPass,
     });
+  };
+  return (
+    //<div style={{ backgroundColor: "#FF0000" }}>red</div>
+    <Container className="body">
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">Bank Sinarmas</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll"></Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      axios
-        .post("https://reqres.in/api/login", {
-          userName: values.userName,
-          password: values.pass,
-        })
-        .then((res) => {
-          localStorage.setItem("token", res.data.token);
-        })
-        .catch((err) => console.error(err));
-    };
-    const handlePassVisibilty = () => {
-      setValues({
-        ...values,
-        showPass: !values.showPass,
-      });
-    };
-    return (
-      //<div style={{ backgroundColor: "#FF0000" }}>red</div>
-      <Container className="body">
-        <Navbar bg="light" expand="lg">
-          <Container fluid>
-            <Navbar.Brand href="#">Bank Sinarmas</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-            
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        
+      <div class="loginForm">
+        <i class="welcomeText">
+          Selamat Datang, silakan login untuk memulai Aplikasi
+        </i>
+        <br />
+        <br />
+        <Form.Group controlId="formText" className="mb-3">
+          <Form.Control type="text" placeholder="Username" />
+        </Form.Group>
 
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
 
-        <div class="loginForm">
-          <i class="welcomeText">
-            Selamat Datang, silakan login untuk memulai Aplikasi
-          </i>
-          <br />
-          <br />
-          <Form.Group controlId="formText" className="mb-3">
-          
-            <Form.Control type="text" placeholder="Username" />
-          </Form.Group>
-          
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-      
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          
-          <div className="d-grid gap-2">
-            <Button variant="danger" size="lg">
-              Block level button
-            </Button>
-          </div>
+        <div className="d-grid gap-2">
+          <Button variant="danger" size="lg">
+            Block level button
+          </Button>
         </div>
-      </Container>
-    );
+      </div>
+    </Container>
+  );
 }
-
-
