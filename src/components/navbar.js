106a06a7bfc5da6f7a7178e2d2e4b71bd5730822
DESKTOP-M6V98NJ\THINKPAD <gerhanaabiwow@gmail.com>
2023-02-14 09:10:44 +0700

    done background login

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,11 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
-function NavbarReact() {
+function NavbarComponent() {
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
@@ -14,7 +14,7 @@ function NavbarReact() {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: "100px" }}
             navbarScroll
           >
             <Nav.Link href="#action1">Home</Nav.Link>
@@ -48,4 +48,4 @@ function NavbarReact() {
   );
 }
 
-export default NavbarReact;
+export default NavbarComponent;
