576ab859cd240913853375107ad84c06bf1f3fa9
Reynaldi Irfan Anwar <reynaldi.i.anwar@banksinarmas.com>
2023-02-14 09:45:26 +0700

    benerin login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,16 +1,18 @@
 import { width } from "@mui/system";
-import { Component, Fragment } from "react";
-import { Container, Row } from "react-bootstrap";
+import axios from "axios";
+import { Component, Fragment, useState } from "react";
+import { Button, Container, Form, Nav, Navbar, Row } from "react-bootstrap";
 import ButtonMain from "../components/button_main";
 import FieldPassword from "../components/field_password";
 import FieldText from "../components/field_text";
 import NavbarReact from "../components/navbar";
 import "./../css/login.css";
 
-export default class Login extends Component {
-  render() {
+import React from 'react'
+
+export default function Login() {
     const [values, setValues] = useState({
-      usernName: "",
+      userName: "",
       pass: "",
       showPass: false,
     });
@@ -36,20 +38,42 @@ export default class Login extends Component {
     return (
       //<div style={{ backgroundColor: "#FF0000" }}>red</div>
       <Container className="body">
-        <NavbarReact />
+        <Navbar bg="light" expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="#">Bank Sinarmas</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+            
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        
+
+
         <div class="loginForm">
           <i class="welcomeText">
             Selamat Datang, silakan login untuk memulai Aplikasi
           </i>
           <br />
           <br />
-          <FieldText />
-          <br />
-          <FieldPassword />
-          <br />
-          <ButtonMain />
+          <Form.Group controlId="formText" className="mb-3">
+          
+            <Form.Control type="text" placeholder="Username" />
+          </Form.Group>
+          
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+      
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          
+          <div className="d-grid gap-2">
+            <Button variant="danger" size="lg">
+              Block level button
+            </Button>
+          </div>
         </div>
       </Container>
     );
-  }
 }
+
+
